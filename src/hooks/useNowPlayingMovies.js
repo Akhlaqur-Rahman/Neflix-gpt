12d787a6b4802039fff6815c92cfc250081ad676
Akhlaqur Rahman <akhlaqurr93@gmail.com>
2026-04-08 11:44:16 +0530

    fix react hook dependency errors

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,10 +1,40 @@
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { API_OPTIONS } from "../utils/constants";
+// import { addNowPlayingMovies } from "../utils/moviesSlice";
+
+// const useNowPlayingMovies = () => {
+//   // Fetch Data from TMDB API and update store
+//   const dispatch = useDispatch();
+
+//   const nowPlayingMovies = useSelector(
+//     (store) => store.movies.nowPlayingMovies
+//   );
+
+//   const getNowPlayingMovies = async () => {
+//     const data = await fetch(
+//       "https://api.themoviedb.org/3/movie/now_playing?page=1",
+//       API_OPTIONS
+//     );
+//     const json = await data.json();
+//     dispatch(addNowPlayingMovies(json.results));
+//   };
+
+//   useEffect(() => {
+//     !nowPlayingMovies && getNowPlayingMovies();
+//   }, [getNowPlayingMovies, nowPlayingMovies]);
+// };
+
+// export default useNowPlayingMovies;
+
+
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const useNowPlayingMovies = () => {
-  // Fetch Data from TMDB API and update store
   const dispatch = useDispatch();
 
   const nowPlayingMovies = useSelector(
@@ -21,8 +51,10 @@ const useNowPlayingMovies = () => {
   };
 
   useEffect(() => {
-    !nowPlayingMovies && getNowPlayingMovies();
-  }, [getNowPlayingMovies, nowPlayingMovies]);
+    if (!nowPlayingMovies) {
+      getNowPlayingMovies();
+    }
+  }, [nowPlayingMovies]); // ✅ FIX
 };
 
 export default useNowPlayingMovies;
--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,10 +1,38 @@
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { API_OPTIONS } from "../utils/constants";
+// import { addPopularMovies } from "../utils/moviesSlice";
+
+// const usePopularMovies = () => {
+//   // Fetch Data from TMDB API and update store
+//   const dispatch = useDispatch();
+
+//   const popularMovies = useSelector((store) => store.movies.popularMovies);
+
+//   const getPopularMovies = async () => {
+//     const data = await fetch(
+//       "https://api.themoviedb.org/3/movie/popular?page=1",
+//       API_OPTIONS
+//     );
+//     const json = await data.json();
+//     dispatch(addPopularMovies(json.results));
+//   };
+
+//   useEffect(() => {
+//     !popularMovies && getPopularMovies();
+//   }, [getPopularMovies, popularMovies]);
+// };
+
+// export default usePopularMovies;
+
+
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addPopularMovies } from "../utils/moviesSlice";
 
 const usePopularMovies = () => {
-  // Fetch Data from TMDB API and update store
   const dispatch = useDispatch();
 
   const popularMovies = useSelector((store) => store.movies.popularMovies);
@@ -19,8 +47,10 @@ const usePopularMovies = () => {
   };
 
   useEffect(() => {
-    !popularMovies && getPopularMovies();
-  }, [getPopularMovies, popularMovies]);
+    if (!popularMovies) {
+      getPopularMovies();
+    }
+  }, [popularMovies]); // ✅ FIX
 };
 
 export default usePopularMovies;
